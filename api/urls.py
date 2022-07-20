efd225a44b4b87906f5dc36436bf404e4595fd66
Bkhyat <sbkhyat@gmail.com>
2022-07-20 16:24:18 +0545

    Create an API for requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,17 @@
 from django.urls import include, path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from api.resume.urls import urlpatterns as resume_url
 
+
+@api_view()
+def get_requirements(request):
+    req_file = open('requirements.txt', 'r')
+    return Response([{"library": x[0], "version": x[1]} for line in req_file for x in [line.strip('\n').split('==')]])
+
+
 urlpatterns = [
-    path(r'resume/', include(resume_url))
+    path(r'resume/', include(resume_url)),
+    path(r'requirements/', get_requirements)
 ]
