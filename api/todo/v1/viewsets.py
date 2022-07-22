11d055c019dd1bdba14b1c963f2e8e315d51fb1d
Bkhyat <sbkhyat@gmail.com>
2022-07-23 00:53:39 +0545

    Implement bulk creation of todos.

--- api/todo/v1/viewsets.py
+++ api/todo/v1/viewsets.py
@@ -8,3 +8,12 @@ from todo.models import ToDo
 class ToDoViewSet(viewsets.ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
