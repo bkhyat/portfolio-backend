6e9bda0291aaf17dd9fedc6b79849764da74596b
Bkhyat <sbkhyat@gmail.com>
2022-10-15 11:58:55 +0545

    Create viewset for TimeLogger with authentication and filter from url queryparam

---
+++ api/timelogger/v1/viewsets.py
@@ -0,0 +1,33 @@
+import django_filters
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import filters
+
+from api.timelogger.v1.serializers import TimeLoggerSerializer
+from timelogger.models import TimeLog
+
+
+class TimeLoggerViewSet(viewsets.ModelViewSet):
+    serializer_class = TimeLoggerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['date']
+    search_fields = ['description']
+    # queryset = TimeLog.objects.all()
+
+    # def filter_queryset(self, queryset):
+    #     pass
+
+    def get_queryset(self):
+        return TimeLog.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid()
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        return super(TimeLoggerViewSet, self).list(request, *args, **kwargs)
+
+
