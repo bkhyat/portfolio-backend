520341ebacca87884ca097d4ea4d78c0471d014e
Bkhyat <sbkhyat@gmail.com>
2022-10-15 22:00:57 +0545

    Include time logger to admin

--- timelogger/admin.py
+++ timelogger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from timelogger.models import TimeLog
+
+
+class TimeLoggerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TimeLog, TimeLoggerAdmin)
