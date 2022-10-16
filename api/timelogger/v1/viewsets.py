b077be6548f742ab26ab8b7de93a340db29b9a09
Bkhyat <sbkhyat@gmail.com>
2022-10-16 20:47:13 +0545

    Create an API for weekly summary for the last and second last weeks based on the records in database

--- api/timelogger/v1/viewsets.py
+++ api/timelogger/v1/viewsets.py
@@ -1,8 +1,12 @@
+from datetime import timedelta
+from itertools import groupby
+
 import django_filters
-from django.db.models import Sum
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
+from django.db.models import Max
 from rest_framework import filters
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.timelogger.v1.serializers import TimeLoggerSerializer
 from timelogger.models import TimeLog
@@ -14,6 +18,7 @@ class TimeLoggerViewSet(viewsets.ModelViewSet):
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['date']
     search_fields = ['description']
+
     # queryset = TimeLog.objects.all()
 
     # def filter_queryset(self, queryset):
@@ -31,4 +36,21 @@ class TimeLoggerViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super(TimeLoggerViewSet, self).list(request, *args, **kwargs)
 
+    @action(methods=['GET'], detail=False, url_path='weekly-summary')
+    def weekly_summary(self, request, *args, **kwargs):
+        """
+        Always considers the maximum date as the current week.
+        ToDo: May need to allow query param to pass two different weeks of the years and filter accordingly
+        """
+        qs = self.get_queryset()
+        last_date = qs.aggregate(Max('date'))['date__max']
+        current = last_date - timedelta(days=last_date.weekday())
+        prev = current - timedelta(days=7)
+        current_week = qs.filter(date__lte=last_date, date__gte=current)
+        prev_week = qs.filter(date__lt=current, date__gte=prev)
 
+        return Response({'this_week': {k: sum(obj.duration_in_minutes for obj in v) for k, v in
+                                       groupby(current_week, key=lambda x: x.date.weekday())},
+                         'prev_week': {k: sum(obj.duration_in_minutes for obj in v) for k, v in
+                                       groupby(prev_week, key=lambda x: x.date.weekday())}
+                         })
